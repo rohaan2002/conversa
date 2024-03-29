b11c3ceccdffa36cd53a64bb0beb4c7ebb8d3561
Rohan <rohan.2021b0131077@abes.ac.in>
2024-03-29 16:16:09 +0530

    when data posted to signup endpoint, successfully generates new user or shows error in json

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,14 +1,71 @@
-export const login =async(req, res)=>{
- 
-}
-export const logout =async(req, res)=>{
-    console.log("logout user");
-    res.send("logoutedd")
-}
+import bcrypt from 'bcryptjs';
+import User from "../models/user.model.js";
+
 export const signup =async(req, res)=>{
     try{
-        const {fullName, username, password, confirmPassword} = req.body
-      }catch(error){
-    
+        const {fullName, userName, password, confirmPassword,gender} = req.body
+        if(password!==confirmPassword){
+            return res.status(400).json({error: "Password do not match"})
+        }
+        const user = await User.findOne({userName})
+        if(user){
+            return res.status(400).json({error:"Username already exists"})
+        }
+
+        // HASH THE PASSWORD USING BCRYPTJS, if you dont anyone with access to your MongoDB database can simply see your password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
+        // avatar profile pic API - https://avatar-placeholder.iran.liara.run/
+
+        // for girl random - https://avatar.iran.liara.run/public/girl
+        // for boy random - https://avatar.iran.liara.run/public/boy
+        // will give diff dp everytime u refresh
+
+        // for same everytime you specify a username(any)-
+        // https://avatar.iran.liara.run/public/boy?username=rohan
+        // would give same dp everytime u refresh
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`
+
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`
+
+
+      //agr sb password verified and username unique to upr dp generate hogi or fir naye user ki detail jayengi acc to User model jo create kia h using userSchema in mongoose (see user.model.js)
+
+        const newUser = new User({
+            fullName: fullName,   //you can write fullName only cz when the key and value are same named, you can just write the value
+             userName:userName,
+             password: hashedPassword,
+             gender: gender,
+            profilePic: (gender==="male")? boyProfilePic: girlProfilePic
+          
+        })
+
+        if(newUser){
+            // Generate JWT token here
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,   //ye _id mongo khud se generate karta  h (jruri hoti bhai ye bhot) 
+            fullName:  newUser.fullName,
+            userName: newUser.userName,
+            profilePic: newUser.profilePic
+        })
+}
+       
+         
+      } catch(error){
+        console.log("some error in signup controller , maybe see if api working correctly or something else", error.message);
+        res.status(500).json({error: `Internal Server Error`})
       }
+}
+
+export const login =(req, res)=>{
+ 
+}
+
+export const logout =(req, res)=>{
+    console.log("logout user");
+    res.send("logoutedd")
 }
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 import express from 'express';
-const app = express();
 import dotenv from 'dotenv';
 import authRoutes from "./routes/authRoutes.js"
 import connectToMongoDB from './db/connectToMongoDB.js';
 
-dotenv.config(); 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res)=>{
-    res.send(`Hii makin waking talkin chat app on port ${PORT}`)
-})
+dotenv.config(); 
+
+app.use(express.json()) //to parse requests with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes);  //middleware
+
+// app.get("/", (req,res)=>{
+//     res.send(`Hii makin waking talkin chat app on port ${PORT}`)
+// })
+
 
 app.listen(PORT, ()=> {
     connectToMongoDB();
