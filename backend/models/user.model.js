5a95d90f232f05759e4155a49208ce80a06fb287
Rohan <rohan.2021b0131077@abes.ac.in>
2024-03-30 01:30:02 +0530

    made message.model.js and conversation.model.js, also made protectRoute middleware to subtract password field off user in further sendings

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -24,8 +24,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: "",
   },
-});
+}, {timestamps:true}  //will have createdAt and updatedAt fields when the new user data will be entered in mongoDB.
+// THIS ACTUALLY WILL DENOTE THE "member since" for the user wrt the chat app membership
+);
 
-const User = mongoose.model("User", userSchema); // you have to give the model name as camelcase singular. mongoDB will make "User" as "users" automatically while making model
+const User = mongoose.model("User", userSchema); // you have to give the model name as camelcase singular. mongoDB will make "User" as "users" automatically while making collection out of this model
 
 export default User;
