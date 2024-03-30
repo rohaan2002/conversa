729e4e5d159770c94aba42267fbd3dce71344905
Rohan <rohan.2021b0131077@abes.ac.in>
2024-03-30 18:31:51 +0530

    made user.controller and userRoutes, which call the function getUsersForSidebar

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/authRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -12,8 +13,10 @@ dotenv.config();
 
 app.use(express.json()) //JSON Parsing: It enables the Express.js application to automatically parse JSON data sent in the body of HTTP requests. When a request is received by the server, if it contains JSON data in its body (checked by content-type: application/json), this middleware parses the JSON data and exposes it in the req.body property of the request object.
 app.use(cookieParser()); //Parsing Cookies: When we talk about parsing cookies, it means extracting the individual cookies and their values from the Cookie header of an incoming HTTP request. This allows the server-side application to access and use the cookies sent by the client. simply find cookie in req.cookie object, instead of accessinh header and then finding
-app.use("/api/auth", authRoutes);  //middleware
-app.use("/api/message", messageRoutes)
+
+app.use("/api/auth", authRoutes);  //middleware for auth functionalities - signup, login, logout
+app.use("/api/message", messageRoutes) //middleware for message related functionalities - sendMessage, getMessage
+app.use("/api/users", userRoutes)
 
 // app.get("/", (req,res)=>{
 //     res.send(`Hii makin waking talkin chat app on port ${PORT}`)
